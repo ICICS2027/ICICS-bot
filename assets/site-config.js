2887c95bdaa0c7d79b26753cc2b4cb6641cc41a7
ICICS2027 <dzym444@gmail.com>
2026-08-27 20:47:13 +0800

    Add files via upload

---
+++ assets/site-config.js
@@ -0,0 +1,16 @@
+// Edit this file when official ICICS 2027 information is confirmed.
+// Leave a value as "To be announced" (or leave a URL/email blank) until it is official.
+window.ICICS_SITE_CONFIG = {
+  conferenceYear: "2027",
+  city: "Chengdu",
+  country: "China",
+  dateLabel: "To be announced",
+  venueName: "To be announced",
+  firstSubmission: "To be announced",
+  firstDecision: "To be announced",
+  secondSubmission: "To be announced",
+  finalDecision: "To be announced",
+  cameraReady: "To be announced",
+  submissionUrl: "",
+  contactEmail: "",
+};
